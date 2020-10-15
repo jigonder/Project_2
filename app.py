50e68e7e6e64d3e692bc6370bf35b92eca4b9e49
Jake Geiser <jgeiser@uncc.edu>
2020-10-15 19:09:08 -0400

    modified app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-# import scrape_news
+import scrape_news
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -12,9 +12,10 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/fin_app")
 @app.route("/")
 def home():
     # Find record from MongoDB and render the template
-    fin_data = "" #mongo.db.collection.find_one() # filter by date
+    news = mongo.db.collection.find_one() # filter by date
+
     # Return template and data
-    return render_template("index.html", newsDoc=fin_data)
+    return render_template("index.html", newsDoc=news)
     
 
 # Route that will trigger the scrape function
@@ -22,13 +23,22 @@ def home():
 def scrape():
 
     # Run the scrape function
-    #fin_data = scrape_news.scrape()
+    news = scrape_news.scrape()
 
     # Update the Mongo database using update and upsert=True (maybe lock to something other than button)
-    # mongo.db.collection.update({}, fin_data, upsert=True)
+    mongo.db.collection.update({}, news, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
+
+@app.route("/miseryindex")
+def misery():
+
+    return render_template("misery.html")
+
+# @app.route("/stockindex")
+# def stocks():
+#     return render_template("stocks.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
