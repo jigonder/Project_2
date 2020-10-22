3684f5852f9a46d5084e2d6da546e7a3e7b70bd2
Jake Geiser <jgeiser@uncc.edu>
2020-10-21 21:47:08 -0400

    fixed issue of not getting latest mongoDB doc

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_news
+import time
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -12,10 +13,14 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/fin_app")
 @app.route("/")
 def home():
     # Find record from MongoDB and render the template
-    news = mongo.db.collection.find_one() # filter by date
+    newsList = mongo.db.collection.find() # find most recent news
+
+    # Run through conditional to see if 
+    for x in newsList:
+        newsOne = x
 
     # Return template and data
-    return render_template("index.html", newsDoc=news)
+    return render_template("index.html", newsDoc=newsOne)
     
 
 # Route that will trigger the scrape function
@@ -27,6 +32,7 @@ def scrape():
 
     # Update the Mongo database using update and upsert=True (maybe lock to something other than button)
     mongo.db.collection.update({}, news, upsert=True)
+    time.sleep(4)
 
     # Redirect back to home page
     return redirect("/")
